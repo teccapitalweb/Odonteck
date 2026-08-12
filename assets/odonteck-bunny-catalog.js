bdcd7bcefa74da7858a2c61b9fd5ee1ccca0490c
TEC Capital web <teccapitalweb@gmail.com>
2026-08-12 13:13:45 -0600

    Add files via upload

---
+++ assets/odonteck-bunny-catalog.js
@@ -0,0 +1,572 @@
+window.ODONTECK_BUNNY_CATALOG = [
+  {
+    "titulo": "Diagnóstico en Ortodoncia para Odontólogos Generales",
+    "collectionId": "3d0a8c29-536d-4c13-b9f8-42fc4681714f",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "56b5a950-60a5-4972-b974-d69bc991b447"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "d9611bfd-f835-4d90-a621-b83537e7bfcc"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "d6d60f9d-8788-4d4c-85f5-3862563cd486"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "4aa4578e-982a-4a8d-a3ab-dfb19636fd47"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "aefca140-1aab-4d67-a1b6-e6ab015d91aa"
+      }
+    ]
+  },
+  {
+    "titulo": "Diagnóstico y Clasificación de la Caries Dental",
+    "collectionId": "d4737d22-01fc-48a3-b263-65b2559abb6a",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "b622fb01-3fce-4f71-b271-4e05a6328ef9"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "03025923-0109-435a-a1a0-78db5a683594"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "bbe71b64-8134-4aa6-abca-7e2ea40a6825"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "ae7a48eb-1cbf-41f4-b384-c2fc2ab3ac60"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "e6cac368-61e9-4f8e-b483-27b002253ac9"
+      }
+    ]
+  },
+  {
+    "titulo": "Endodoncia Pediátrica Manejo de los Canales Radiculares en Niños",
+    "collectionId": "8fb78166-3d21-4f0e-90a5-c6d01083fcda",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "014d4830-df4e-4fb1-b910-646dfc8fe66d"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2 y 3",
+        "provider": "bunny",
+        "videoId": "3dc47289-be96-4cbf-a489-80d7e2ef4af9"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 4 y 5",
+        "provider": "bunny",
+        "videoId": "eadb58a0-71d4-466f-b2a2-f0f7e5ed3e75"
+      }
+    ]
+  },
+  {
+    "titulo": "Extracciones Simples",
+    "collectionId": "7e5cc91e-1207-4644-995b-887a699daec5",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "fe861c3c-5b80-4b59-90fa-68afaa229843"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "1522e987-12b8-48fa-bdab-01147c373c31"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "5903f147-3c7b-4c76-9f83-ef06bcb8e917"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "74db63ae-7b11-4857-9a9e-0da59e462edf"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "1d36f6c3-fd92-4c57-a831-1b253d8f63e6"
+      }
+    ]
+  },
+  {
+    "titulo": "Férulas de Contención en Odontopediatría Tipos, Tiempos y Materiales",
+    "collectionId": "f96bbad0-7c1a-4dfa-8abb-0b7e68823ede",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1 y 2",
+        "provider": "bunny",
+        "videoId": "fb15e281-bbfe-45b3-b7ca-2a08fa0da60c"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 3 y 4",
+        "provider": "bunny",
+        "videoId": "0a782c7c-22ea-4ab4-bd0e-3b86ef2de71d"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "af9c8c6e-199b-40e2-a80f-fbc0ec14b33c"
+      }
+    ]
+  },
+  {
+    "titulo": "Manejo de Abscesos Periodontales",
+    "collectionId": "657d2041-bc8b-462a-b2f1-edd50eca0ce6",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "d3e06e61-3b53-41d3-9091-45c2e5bc7696"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "e027ac12-bd23-4a2b-981d-9adfb2c0d52b"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "eeeb101e-dc66-4281-b38d-813d83bd9f31"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "8768948c-56da-4ec2-93a9-5ee50f3f7de7"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "a996a08f-a88b-46e1-9e53-b50121141d34"
+      }
+    ]
+  },
+  {
+    "titulo": "Materiales Dentales Restauradores Indicaciones y Usos",
+    "collectionId": "21be27bb-5673-4c2c-903f-fd9852336e95",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "fa308fcb-4168-4283-a421-7148b6bb9464"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "d1c382a8-50a2-43fe-a5d9-fc239d10c2dc"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "61ef3b83-d15f-4db5-98a7-b6da6a11779e"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4 y 5",
+        "provider": "bunny",
+        "videoId": "97839549-e318-4b14-ba4b-e6ea3e5a915e"
+      }
+    ]
+  },
+  {
+    "titulo": "Ortodoncia Lingual y Estética",
+    "collectionId": "4d80f182-0ce4-40a4-a589-9ba15d10a280",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "cb48abda-dee5-4e9c-9bec-b075aca914ca"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "ab0de553-7249-449f-9b89-62d73bc00a13"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3 y 4",
+        "provider": "bunny",
+        "videoId": "494c41bb-4785-4661-90dc-c20f2b6cd094"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "46c005a1-862d-4cd0-9e9a-4b179e258a35"
+      }
+    ]
+  },
+  {
+    "titulo": "Patologías Dentales Más Comunes en la Infancia",
+    "collectionId": "5aede94d-1a75-4b73-ba4c-8b1a74b62239",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "78a7f1dc-c958-49eb-911f-b16cb3a12ee9"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "043107a0-bec1-4f21-96be-dab0a3a68f43"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "3c4d013a-a8de-48af-8014-8ffb153c6c88"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4 y 5",
+        "provider": "bunny",
+        "videoId": "5e665c80-3ffc-4250-a37c-0834ad1aae13"
+      }
+    ]
+  },
+  {
+    "titulo": "Periodoncia Estética Manejo de Encía y Diseño de Sonrisa",
+    "collectionId": "2d306d08-46ca-40c2-a6e2-2e27a33a67a7",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "092b300c-8f79-488b-9a3a-1db6cac46631"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "86a813da-9181-4ea4-9b3f-289f12998cd7"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "13b3f986-7db4-4fc9-94cf-b1b82b8c081b"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "abded854-bb1a-409f-9873-824de2401fd4"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "a044a820-2501-4ba6-9c0f-991d5d649249"
+      }
+    ]
+  },
+  {
+    "titulo": "Periodoncia estética y plástica",
+    "collectionId": "88fbd011-8658-4b83-b83b-fa735f8a3895",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "31df895f-26bd-48ee-a13b-89a188711929"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "f70bfdc5-0e68-468d-a981-b5e0b217c119"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3 y 4",
+        "provider": "bunny",
+        "videoId": "09150bea-a21e-4a4d-85ae-9eb9491f3aa2"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "08e2fd9c-0d7f-4603-9368-eec7c8054dd3"
+      }
+    ]
+  },
+  {
+    "titulo": "Raspado y Alisado Radicular",
+    "collectionId": "445daaed-8a70-4908-b474-71bb1c74ba19",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "07883ca5-01fa-47c9-9075-29d3ec59a050"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "3fcaa0e8-5299-4a41-9205-aead6a744d96"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "42599f47-1473-415f-bb55-f6b9ae3f58ce"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "1108cb6d-7dff-476d-b62f-bea9b0a1ab85"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "3499ce6f-d858-4e1f-bb18-a01d6065bd09"
+      }
+    ]
+  },
+  {
+    "titulo": "Rehabilitación Oral Estética con Coronas y Puentes",
+    "collectionId": "0868ec87-fa43-4f89-98ff-298f688e2814",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1 y 2",
+        "provider": "bunny",
+        "videoId": "918b434c-10c4-48e2-8945-055625ea9c30"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 3 y 4",
+        "provider": "bunny",
+        "videoId": "b3d25bdd-ae75-45b7-b6d2-b3ad09d7020e"
+      },
+      {
+        "numero": 3,
+        "titulo": "CLASE 5",
+        "provider": "bunny",
+        "videoId": "04fd2fb5-c257-4680-93a4-691d7286826e"
+      }
+    ]
+  },
+  {
+    "titulo": "Resinas Compuestas Selección, Manipulación y Técnicas de Estratificación",
+    "collectionId": "470a3ec4-e5c3-44d4-8667-e352bb06bb26",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "55c270c3-34e8-4485-a7c6-51170f697242"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "2640a818-3aab-46cb-90a3-ab0eb257c98b"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "92eb5c79-7471-4fae-9ed9-5d7547127e3d"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "b8aeb085-f701-4a79-a405-1c9c0b6b8fd7"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "7a14170a-fd49-4d2d-aee7-3b94f676ab12"
+      }
+    ]
+  },
+  {
+    "titulo": "Retroobturación en Endodoncia Técnicas y Materiales",
+    "collectionId": "6a3ee381-8e40-40c8-a5d5-d7993e90082d",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "d315eb19-42ec-4ca9-bba8-b4cde09882f1"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "eda1e5fb-c0cf-4bd3-ab96-82cb6dcfd180"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "cc342c87-16c7-4ab6-a346-f6ff0342986a"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "1e8b1b16-8715-4fd0-85fc-d32bc61d2d26"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "cc45cade-8592-4f4a-9f5a-5f93fe58aa16"
+      }
+    ]
+  },
+  {
+    "titulo": "Salud Oral en Pacientes con Condiciones Sistémicas",
+    "collectionId": "3c6a8571-db02-469f-bfbe-c33e2dbd4863",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "03f2701a-a00c-4938-8e81-bc114ef1f951"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "7e5d6a6b-5fb7-4f2d-a1c7-7d40599b688e"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "566819fe-c16d-4092-a7fe-cee91bfe5f62"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4 y 5",
+        "provider": "bunny",
+        "videoId": "c69094b6-637e-44bb-acd5-c85f62fb2091"
+      }
+    ]
+  },
+  {
+    "titulo": "Tratamientos Periodontales No Quirúrgicos Técnicas Clínicas para el Control de la Enfermedad Periodontal",
+    "collectionId": "9fa116e3-8d3b-4cbd-b7e4-12996f13578a",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1",
+        "provider": "bunny",
+        "videoId": "ee093e87-d700-4ec8-9557-2af205dc88de"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 2",
+        "provider": "bunny",
+        "videoId": "3240de44-12d5-463a-b475-7ffc2a61c0b0"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "5a518b6f-b9d9-4096-8a27-2845d67a21b5"
+      },
+      {
+        "numero": 4,
+        "titulo": "Clase 4",
+        "provider": "bunny",
+        "videoId": "52232044-db43-41f1-bc64-a98d9b870f42"
+      },
+      {
+        "numero": 5,
+        "titulo": "Clase 5",
+        "provider": "bunny",
+        "videoId": "30acf1f0-9844-406f-bd9b-f246552b3b12"
+      }
+    ]
+  },
+  {
+    "titulo": "Traumatismos Dentales en Niños",
+    "collectionId": "3fc58e6c-7918-4f3c-b9fe-74c04bef810d",
+    "sesiones": [
+      {
+        "numero": 1,
+        "titulo": "Clase 1 y 2",
+        "provider": "bunny",
+        "videoId": "c7f07a89-401c-47c8-bce3-382dc9346e3b"
+      },
+      {
+        "numero": 2,
+        "titulo": "Clase 3",
+        "provider": "bunny",
+        "videoId": "594ca606-eb5a-414f-8467-a0e89a9b7e95"
+      },
+      {
+        "numero": 3,
+        "titulo": "Clase 4 y 5",
+        "provider": "bunny",
+        "videoId": "4ee74a2c-f16d-414b-9b4b-733bf577a1d1"
+      }
+    ]
+  }
+];
